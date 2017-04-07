750d33b2f7066af7c0d13bc416b3ec6708d81f03
Glenn McEwan <glenn.mcewan@theuniprogroup.com>
2017-04-07 14:20:10 +0100

    Add Simple calculator class so I can test Behat's basic behaviour before plugging in Selenium

---
+++ src/Calculator.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App;
+
+use Exception;
+
+class Calculator
+{
+	/**
+	 * The total value stored in the calculator.
+	 * Keeps a record when adding and subtracting.
+	 * @var int
+	 */
+	protected $sum;
+
+	/**
+	 * Initialise class properties.
+	 */
+	public function __construct()
+	{
+		$this->sum = 0;
+	}
+
+	/**
+	 * Clear the current total.
+	 */
+	public function clear()
+	{
+		$this->total = 0;
+	}
+
+	/**
+	 * Get the current total.
+	 * 
+	 * @return int
+	 */
+	public function getSum()
+	{
+		return $this->sum;
+	}
+
+	/**
+	 * Add a number to the total.
+	 * @param int $int The number to add to the total.
+	 */
+	public function add($int)
+	{
+		if (!is_int($int)) {
+			throw new Exception('Calculator needs integers.');
+		}
+
+		$this->sum += $int;
+	}
+
+	/**
+	 * Subtract a number from the total.
+	 * @param  int $integer The number to subtract from the total.
+	 */
+	public function subtract($int)
+	{
+		if (!is_int($int)) {
+			throw new Exception('Calculator needs integers.');
+		}
+
+		$this->sum -= $int;
+	}
+
+	/**
+	 * Magic __toString method, called when the Calculator object is cast to a string.
+	 *
+	 * @return string Calulator total as a string
+	 */
+	public function __toString()
+	{
+		return (string) $this->getSum();
+	}
+}
