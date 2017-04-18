1018b1f1ecc7e1dd2a4ffe9e6c2e066e93ce6c57
Glenn McEwan <glenn.mcewan@theuniprogroup.com>
2017-04-18 10:09:37 +0100

    PHPCS

--- features/bootstrap/CalculatorContext.php
+++ features/bootstrap/CalculatorContext.php
@@ -6,80 +6,81 @@ use Behat\Gherkin\Node\TableNode;
 
 use App\Calculator;
 
-class CalculatorContext implements Context
-{
-    /**
-     * App\Calculator instance
-     * @var App\Calculator
-     */
-    protected $calculator;
+/**
+ * Calculator context
+ */
+class CalculatorContext implements Context {
 
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * App\Calculator instance.
+   *
+   * @var App\Calculator
+   */
+  protected $calculator;
 
-    /**
-     * @Transform /^(-?[0-9]+)$/
-     * @param  string $string String representation of a number
-     * @return int            Converted the string to an int type.
-     */
-    public function castStringToNumber($string)
-    {
-        return intval($string);
-    }
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct() {
+  }
 
-    /**
-     * @Given /^I have a new calculator instance$/
-     */
-    public function iHaveANewCalculatorInstance()
-    {
-        $this->calculator = new Calculator;
-    }
+  /**
+   * Behat Transformer method for numbers.
+   *
+   * @param string $string
+   *   String representation of a number.
+   *
+   * @return int
+   *   Converted the string to an int type.
+   *
+   * @Transform /^(-?[0-9]+)$/
+   */
+  public function castStringToNumber($string) {
+    return intval($string);
+  }
 
-    /**
-     * @When /^I add the number (-?[0-9]+)$/
-     */
-    public function iAddTheNumber($number)
-    {
-        $this->calculator->add($number);
-    }
+  /**
+   * @Given /^I have a new calculator instance$/
+   */
+  public function iHaveANewCalculatorInstance() {
+    $this->calculator = new Calculator();
+  }
 
-    /**
-     * @When /^I add the number (-?[0-9]+) and the number (-?[0-9]+)$/
-     */
-    public function iAddTheNumberAndTheNumber($first, $second)
-    {
-        $this->calculator->add($first);
-        $this->calculator->add($second);
-    }
+  /**
+   * @When /^I add the number (-?[0-9]+)$/
+   */
+  public function iAddTheNumber($number) {
+    $this->calculator->add($number);
+  }
 
-    /**
-     * @When /^I subtract the number (-?[0-9]+)$/
-     */
-    public function iSubtractTheNumber($number)
-    {
-        $this->calculator->subtract($number);
-    }
+  /**
+   * @When /^I add the number (-?[0-9]+) and the number (-?[0-9]+)$/
+   */
+  public function iAddTheNumberAndTheNumber($first, $second) {
+    $this->calculator->add($first);
+    $this->calculator->add($second);
+  }
 
-    /**
-     * @Then /^I should see a total of (-?[0-9]+)$/
-     */
-    public function iShouldSeeATotalOf($expected)
-    {
-        $actual = $this->calculator->getSum();
+  /**
+   * @When /^I subtract the number (-?[0-9]+)$/
+   */
+  public function iSubtractTheNumber($number) {
+    $this->calculator->subtract($number);
+  }
 
-        if ($actual !== $expected) {
-            throw new Exception('Actual total: ' . $actual);
-        }
+  /**
+   * @Then /^I should see a total of (-?[0-9]+)$/
+   */
+  public function iShouldSeeATotalOf($expected) {
+    $actual = $this->calculator->getSum();
 
-        return $actual;
-    }
+    PHPUnit_Framework_Assert::assertSame($expected, $actual);
+
+    return $actual;
+  }
+
 }
--- src/Calculator.php
+++ src/Calculator.php
@@ -4,74 +4,80 @@ namespace App;
 
 use Exception;
 
-class Calculator
-{
-    /**
-     * The total value stored in the calculator.
-     * Keeps a record when adding and subtracting.
-     * @var int
-     */
-    protected $sum;
+/**
+ * Simple calculator class.
+ */
+class Calculator {
 
-    /**
-     * Initialise class properties.
-     */
-    public function __construct()
-    {
-        $this->sum = 0;
+  /**
+   * The total value stored in the calculator.
+   *
+   * Keeps a record when adding and subtracting.
+   *
+   * @var int
+   */
+  protected $sum;
+
+  /**
+   * Initialise class properties.
+   */
+  public function __construct() {
+    $this->sum = 0;
+  }
+
+  /**
+   * Clear the current total.
+   */
+  public function clear() {
+    $this->sum = 0;
+  }
+
+  /**
+   * Get the current total.
+   *
+   * @return int
+   *   Current total
+   */
+  public function getSum() {
+    return $this->sum;
+  }
+
+  /**
+   * Add a number to the total.
+   *
+   * @param int $int
+   *   The number to add to the total.
+   */
+  public function add($int) {
+    if (!is_int($int)) {
+      throw new Exception('Calculator needs integers.');
     }
 
-    /**
-     * Clear the current total.
-     */
-    public function clear()
-    {
-        $this->sum = 0;
+    $this->sum += $int;
+  }
+
+  /**
+   * Subtract a number from the total.
+   *
+   * @param int $int
+   *   The number to subtract from the total.
+   */
+  public function subtract($int) {
+    if (!is_int($int)) {
+      throw new Exception('Calculator needs integers.');
     }
 
-    /**
-     * Get the current total.
-     * 
-     * @return int
-     */
-    public function getSum()
-    {
-        return $this->sum;
-    }
+    $this->sum -= $int;
+  }
 
-    /**
-     * Add a number to the total.
-     * @param int $int The number to add to the total.
-     */
-    public function add($int)
-    {
-        if (!is_int($int)) {
-            throw new Exception('Calculator needs integers.');
-        }
+  /**
+   * Magic method, called when the Calculator object is cast to a string.
+   *
+   * @return string
+   *   Calulator total as a string
+   */
+  public function __toString() {
+    return (string) $this->getSum();
+  }
 
-        $this->sum += $int;
-    }
-
-    /**
-     * Subtract a number from the total.
-     * @param  int $int The number to subtract from the total.
-     */
-    public function subtract($int)
-    {
-        if (!is_int($int)) {
-            throw new Exception('Calculator needs integers.');
-        }
-
-        $this->sum -= $int;
-    }
-
-    /**
-     * Magic __toString method, called when the Calculator object is cast to a string.
-     *
-     * @return string Calulator total as a string
-     */
-    public function __toString()
-    {
-        return (string) $this->getSum();
-    }
 }
